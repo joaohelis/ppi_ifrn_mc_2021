3a491f8f76bae211d6a6499eae0ccc79ebb5e755
João Helis Bernardo <joaohelis.bernardo@gmail.com>
2021-06-24 15:54:08 -0300

    Projeto Desenvolvido na Aula do dia 24/06/2020

--- minhaescola/app/controllers/Alunos.java
+++ minhaescola/app/controllers/Alunos.java
@@ -17,13 +17,48 @@ public class Alunos extends Controller{
 		render(alunos);
 	}
 	
-	public static void salvar(String nome, String matricula, 
-			String cpf, Date dataNascimento) {		
-		Aluno aluno = new Aluno();
-		aluno.nome = nome;
-		aluno.cpf = cpf;
-		aluno.matricula = matricula;
-		aluno.dataNascimento = dataNascimento;
+	public static void editar(Long id) {
+		Aluno aluno = Aluno.findById(id);
+		renderTemplate("Alunos/form.html", aluno);
+	}
+	
+//  EXEMPLO DE LISTAGEM COM ENTIDADES UTILIZANDO EXCLUSÃO LÓGICA
+//	public static void listar() {
+//		List<Aluno> alunos = Aluno.find("ativo", true).fetch();
+//		render(alunos);
+//	}
+	
+	public static void remover(Long id) {
+		Aluno aluno = Aluno.findById(id);
+		aluno.delete();
+		listar();
+	}
+	
+	// EXEMPLO DE EXCLUSÃO LÓGICA
+//	public static void remover(Long id) {
+//		Aluno aluno = Aluno.findById(id);
+//		aluno.ativo = false;
+//		aluno.save();
+//		listar();
+//	}
+	
+//	public static void salvar(String nome, String matricula, 
+//			String cpf, Date dataNascimento, Long id) {
+//		Aluno aluno;
+//		if(id == null)
+//			aluno = new Aluno();
+//		else
+//			aluno = Aluno.findById(id);		
+//		aluno.id = id;
+//		aluno.nome = nome;
+//		aluno.cpf = cpf;
+//		aluno.matricula = matricula;
+//		aluno.dataNascimento = dataNascimento;
+//		aluno.save();
+//		listar();
+//	}
+	
+	public static void salvar(Aluno aluno) {
 		aluno.save();
 		listar();
 	}
--- minhaescola/app/models/Aluno.java
+++ minhaescola/app/models/Aluno.java
@@ -13,6 +13,11 @@ public class Aluno extends Model {
 	public String cpf;
 	public String matricula;
 	public Date dataNascimento;
+	public boolean ativo;
+	
+	public Aluno() {
+		this.ativo = true;
+	}
 	
 	public int calcularIdade() {		
 		Date dataAtual = new Date();
